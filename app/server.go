a53def957cd1f62ec6e205831678d86a891edd9c
anish-pice <anish@pice.one>
2024-02-17 00:54:12 +0530

    test

--- app/server.go
+++ app/server.go
@@ -11,25 +11,29 @@ func main() {
 	fmt.Println("Logs from your program will appear here!")
 
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
-	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
-		os.Exit(1)
-	}
+	handleErr(err)
+	defer l.Close()
 
 	for {
 
 		conn, err := l.Accept()
-		if err != nil {
-			fmt.Println("Error accepting connection: ", err.Error())
-			os.Exit(1)
-		}
+		handleErr(err)
+		defer conn.Close()
 
-		n, err := conn.Write([]byte("+PONG\r\n"))
-		if err != nil {
-			fmt.Println(err.Error())
-			os.Exit(1)
-		}
+		b := make([]byte, 1024)
+		n, err := conn.Read(b)
+
+		n, err = conn.Write([]byte("+PONG\r\n"))
+		handleErr(err)
+
 		log.Printf("received %d bytes", n)
 	}
 
+}
+
+func handleErr(err error) {
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
 }
